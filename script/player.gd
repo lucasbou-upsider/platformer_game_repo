3ab82ce8b3afe8bb92a202f05985a895bd864918
lucasbou <lucasbou2010@gmail.com>
2025-11-12 21:50:22 +0100

    ajout du wall jump

--- script/player.gd
+++ script/player.gd
@@ -13,22 +13,38 @@ var courir = false
 @onready var particule_course: CPUParticles2D = $particule_course
 
 #saut
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -600.0
 @onready var jump_timer: Timer = $jump_timer
 var can_jump = false
 var coyote_time = 0.3
 @export_range(0, 1) var deceleration = 0.1
 @export_range(0, 1) var aceleration = 0.1
+@onready var jump_buffer_timer: Timer = $jump_buffer_timer
+var saut = 0
+const  gravity = 1000
+const  fall_gravity = 1400
+var nbr_de_saut = 0
+
+#wall jump
+@export_category("wall jump variable")
+@export var wall_slide = 150
+@onready var left_ray: RayCast2D = $raycast/left_ray
+@onready var right_ray: RayCast2D = $raycast/right_ray
+@export var wall_x_force = 200
+@export var wall_y_force = -700
+var is_wall_jumping = false
 
 func _physics_process(delta: float) -> void:
 	
+	wall_logique()
 	ui_platforme()
 	mort()
 	platforme_reload()
+	flip()
 	
 	# gravité
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y +=  get_good_gravity() * delta
 
 	#coyote time
 	if is_on_floor() and can_jump == false:
@@ -42,13 +58,29 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("saut") and can_jump == true:
 		can_jump = false
 		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_released("saut") and velocity.y < 0:
+		#jump_buffer_timer.start()
+		#saut = 0
+	#if Input.is_action_just_pressed("saut") and can_jump == true:
+		#can_jump = false
+		#jump_buffer_timer.start()
+		#saut = 1
+	#if is_on_floor() and !jump_buffer_timer.is_stopped():
+		#if saut == 0:
+			#print("petit_saut")
+			#velocity.y = JUMP_VELOCITY / 4
+		#if saut == 1:
+			#print("grand_saut")
+			#velocity.y = JUMP_VELOCITY
+		
 
 	#direction
-	var direction := Input.get_axis("gauche", "droite")
-	if direction:
-		velocity.x = move_toward(velocity.x , direction * speed, aceleration * speed)
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed * deceleration)
+	if is_wall_jumping == false:
+		var direction := Input.get_axis("gauche", "droite")
+		if direction:
+			velocity.x = move_toward(velocity.x , direction * speed, aceleration * speed)
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed * deceleration)
 
 	#course
 	if Input.is_action_just_pressed("courire"):
@@ -63,10 +95,44 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+#wall jump
+func wall_logique():
+	if is_on_wall_only():
+		velocity.y = wall_slide
+		if Input.is_action_just_pressed("saut"):
+			#if left_ray.is_colliding():
+				#velocity = Vector2(wall_x_force, wall_y_force)
+				#wall_jumping()
+			if right_ray.is_colliding():
+				velocity = Vector2(-wall_x_force, wall_y_force)
+				wall_jumping()
+func wall_jumping():
+	is_wall_jumping = true
+	await get_tree().create_timer(0.12).timeout
+	is_wall_jumping = false
 
+
+#flip players
+func flip():
+	if velocity.x > 0.0:
+		scale.x = scale.y * 1
+		wall_x_force = 300.0
+	if velocity.x < 0.0:
+		scale.x = scale.y * -1
+		wall_x_force = -300.0
+
+
+#gravity saut
+func get_good_gravity():
+	if velocity.y < 0:
+		return gravity
+	return fall_gravity
+
+#coyot time
 func _on_jump_timer_timeout() -> void:
 	can_jump = false
 
+#mort
 func mort():
 	if GameManager.player_mort == true:
 		global_position = GameManager.respawn_point
@@ -74,10 +140,11 @@ func mort():
 		print("mort")
 		#aaaaa
 
-
+#comptage du nbr de platforme ui
 func ui_platforme():
 	ui_platforme_animation.play(str(GameManager.nbr_platforme))
 
+#permet de recevoi les platforme a la fin du timer
 func platforme_reload():
 	if GameManager.nbr_platforme == 0:
 		progresse_bar_timer.start()
