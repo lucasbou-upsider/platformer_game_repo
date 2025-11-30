a495c55c763dab14d9b1c1d9fbf1db5bbe94150e
lucasbou <lucasbou2010@gmail.com>
2025-11-30 12:55:40 +0100

    scene changer

--- script/scene_transition.gd
+++ script/scene_transition.gd
@@ -1,14 +1,19 @@
 extends Area2D
 
 @export var scene: PackedScene 
-@export var sortie = 0
-
+@export var current_scene: PackedScene
+@export var distance_sortie: int
+@onready var player_pos: player = $"../player"
 
 func _ready() -> void:
+	scene = scene
+	if scene == GameManager.sortie:
+		player_pos.global_position = global_position + Vector2(distance_sortie , 0)
 	print(scene)
-	GameManager.sortie = sortie
+	
 
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
+		GameManager.sortie = current_scene
 		get_tree().change_scene_to_packed(scene)
