5dcf60306d2f963253c1541be6737d399cb260de
lucasbou <lucasbou2010@gmail.com>
2025-12-11 20:22:24 +0100

    stalactique + refonte sprite

---
+++ script/world_1/goutte_de_vide.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _ready() -> void:
+	pass 
+
+
+
+func _process(delta: float) -> void:
+	if ray_cast_2d.is_colliding():
+		freeze = true
+		animated_sprite_2d.play("splash")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	queue_free()
---
+++ script/world_1/stalactite.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@onready var marker_2d: Marker2D = $Marker2D
+var instance_goutte = preload("res://scene/world_1/goutte_de_vide.tscn")
+@onready var timer: Timer = $Timer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func invoc_void():
+	var goutte_vide = instance_goutte.instantiate()
+	goutte_vide.position = marker_2d.position
+	add_child(goutte_vide)
+
+
+func _on_timer_timeout() -> void:
+	animated_sprite_2d.play("goutte")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	timer.start()
+	invoc_void()
